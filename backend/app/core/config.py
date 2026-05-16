a5c269504d4c67799cdba811509d831ed6cb9e06
Tayebe kavousi <tayebekavousi68@gmail.com>
2026-05-16 17:46:48 +0100

    adding dockerignore files

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     # DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://admin:securepassword123@localhost:5432/vectordb")
     DATASET_PATH: str = os.getenv("DATASET_PATH", "data/messy_cloud_billing.csv")
     # LANGCHAIN_DB_URL: str = os.getenv("LANGCHAIN_DB_URL", "postgresql+psycopg2://admin:securepassword123@localhost:5432/vectordb")
-    OLLAMA_BASE_URL: str = os.getenv("OLLAMA_BASE_URL", "http://192.168.1.214:11434")
+    OLLAMA_BASE_URL: str = os.getenv("OLLAMA_BASE_URL", "http://192.168.1.135:11434")
     COLLECTION_NAME: str = "health_documents"
     UPLOAD_DIR: str = "uploaded_docs"
 
